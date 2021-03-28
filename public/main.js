12d6b9744f5a4f7923c3d13c02a1ea75186b462a
Arb Rahim Badsa <34975329+Arbrahimbadsa@users.noreply.github.com>
2021-03-28 13:34:17 +0600

    Dual player setup

--- public/main.js
+++ public/main.js
@@ -1,23 +1,57 @@
 // main socket instance
 const socket = io();
 // on connect (clinet side)
-socket.on('connect', () => {
-    console.log(socket.id);
-});
 const box = document.getElementById('box');
-// on newuser
-socket.on('dataFromServer', (data) => {
-    const pos = data.data;
-    // update data for other players
-    box.style.left = pos.x + 'px';
-    box.style.top = pos.y + 'px';
-    console.log(pos);
-});
-
-window.onmousemove = (e) => {
-    const pos = {x: e.clientX, y: e.clientY};
-    // update the client
-    box.style.left = pos.x + 'px';
-    box.style.top = pos.y + 'px';
-    socket.emit('posFromClient', pos);
+const message = document.getElementById('message');
+const updates = document.getElementById('updates');
+const createButtons = (count) => {
+    const p = document.getElementById('parent');
+    for (let i = 0; i < count; i++) {
+        const button = document.createElement('button');
+        button.classList.add('gameButton');
+        button.innerText = i + 1;
+        p.append(button);
+    }
 }
+const setLogic = (senderId) => {
+    const buttons = document.getElementsByClassName('gameButton');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', () => {
+            socket.emit('updates', {sender: senderId, selected: i + 1});
+        });
+    }
+}
+// on connecting a new user.
+socket.on('connect', () => {
+    socket.emit('firstEntry', window.location.href);
+});
+// generate the invite id for player two to come
+socket.on('inviteId', id => {
+    //invite link
+    message.innerText = 'Waiting for player two to join...';
+    const url = `<p><b>Share your link: </b>http://localhost:5000/invite/${id}</p>`;
+    box.innerHTML = url;
+});
+// now if we are connected with player two
+socket.on('player_one_got_player_two', playerTwoId => {
+    socket.emit('player_two_got_player_one', playerTwoId);
+});
+// game starts for player two
+socket.on('game_starts_for_player_two', playerOneId => {
+    message.innerText = `Connected with ${playerOneId}`;
+    socket.emit('player_one_got_player_two', playerOneId);
+    createButtons(10);
+    setLogic(playerOneId);
+});
+// game starts for player one
+socket.on('game_starts_for_player_one', playerTwoId => {
+    message.innerText = `Connected with ${playerTwoId}`;
+    box.innerText = '';
+    createButtons(10);
+    setLogic(playerTwoId);
+});
+// game updates
+socket.on('gameUpdates', (data) => {
+    const update = `<p><b>${data.sender}: </b> clicked ${data.selected}</p>`;
+    updates.innerHTML += update;
+});
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 // init scoket.io with express
 const http = require('http');
 const httpServer = http.createServer(app);
 const io = require('socket.io')(httpServer);
 
+
+// body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 // server static files so that the server can access the files.
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -16,14 +20,38 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/main.html'));
 });
 
+// invite link route
+app.get('/invite/:id', (req, res) => {
+    const inviteId = req.params.id;
+    res.sendFile(path.join(__dirname, 'views/main.html'));
+});
+
 // on a new connection
 io.on('connection', (socket) => {
-    // data flow => first a client comes > clicks on the button > client emits an event > 
-    // sever listens on the event > server emits a new event > client listens on the event.
-    // listen from client
-    socket.on('posFromClient', data => {
-        // broadcast to all clients including sender
-        socket.broadcast.emit('dataFromServer', {sender: socket.id, data: {x: data.x, y: data.y}});
+    socket.on('firstEntry', data => {
+        const idExists = data.split('/').length > 4;
+        if (idExists) {
+            const id = data.split('/')[data.split('/').length - 1]; // player one id.
+            // we emit to player one that we have got player two.
+            // this socket.id is the id of the new user who got the link from player one.
+            // emit this player id
+            socket.to(id).emit('player_one_got_player_two', socket.id);
+        } else {
+            // no id exists means this is the first player.
+            // emit the first player id.
+            socket.emit('inviteId', socket.id);
+        }
+    });
+    socket.on('player_two_got_player_one', playerTwoId => {
+        // let player two know that now it's time to play.
+        socket.to(playerTwoId).emit('game_starts_for_player_two', socket.id);
+    });
+    socket.on('player_one_got_player_two', playerOneId => {
+        // let player one know game has started for player two and prepare player first two start
+        socket.to(playerOneId).emit('game_starts_for_player_one', socket.id);
+    });
+    socket.on('updates', data => {
+        io.to(data.sender).emit('gameUpdates', {sender: socket.id, selected: data.selected});
     });
 });
 
